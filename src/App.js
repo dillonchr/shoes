93ec92c76ba6e26c8424967052ab3693aecdb7a7
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-02-15 21:00:30 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Shoes</h1>
+          <p>Shoes say a lot about a person. I tried to find credible articles to back up this claim. I can't find anything beyond listicles that are pushing shoes off on personality quiz hungry people. Oh well. I happen to agree with it and give it the proper amount of credence. Little more than finding it funny.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
