bbe59fdbee3663cf3d8b7dadfbf91058ca5b4880
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-02-16 22:40:56 -0600

    finished rough UI changes

--- src/components/shoes-list.js
+++ src/components/shoes-list.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './shoes-list.css';
+import ShoeDetail from './shoe-detail';
 
 const Shoe = ({
-    thumbUrl,
-    name
-}) => (
-    <div className="shoes-list__tile">
-        <img src={thumbUrl} alt={name} />
+                  thumbUrl,
+                  name,
+                  onClick
+              }) => (
+    <div className="shoes-list__tile" onClick={onClick}>
+        <img src={thumbUrl} alt={name}/>
+        <p className="shoes-list__label">{name}</p>
     </div>
 );
 
 export default class ShoesList extends React.PureComponent {
     state = {
-        shoes: []
+        shoes: [],
+        selectedShoe: -1
     };
 
     componentWillMount() {
@@ -24,13 +28,21 @@ export default class ShoesList extends React.PureComponent {
                 throw new Error(`fetch error: ${r.status}`);
             })
             .then(shoes => {
-                this.setState({ shoes });
+                this.setState({shoes});
             });
     }
 
+    showShoeDetails = i => this.setState({selectedShoe: i});
+
+    showShoesListAgain = () => this.showShoeDetails(-1);
+
     getShoes() {
-        return this.state.shoes.map(s => {
-            return <Shoe key={s.name} name={s.name} thumbUrl={`images/thumbs/${s.image}`} />;
+        if (this.state.selectedShoe >= 0) {
+            return <ShoeDetail shoe={this.state.shoes[this.state.selectedShoe]} onExit={this.showShoesListAgain}/>;
+        }
+        return this.state.shoes.map((s, i) => {
+            return <Shoe key={s.name} name={s.name} onClick={() => this.showShoeDetails(i)}
+                         thumbUrl={`images/thumbs/${s.image}`}/>;
         });
     }
 
